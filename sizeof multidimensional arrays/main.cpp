a4f7d6ce3109865850a1fac35858730390e6d621
nljor <nljor@JONSSONS>
2017-03-02 16:23:22 +0100

    Add project files.

---
+++ sizeof multidimensional arrays/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+
+	string animals[][3] = {
+		{"fox", "dog", "cat"},
+		{"mouse", "squirrel", "parrot"}
+	};
+
+	cout << sizeof(animals[0]) << endl;
+
+	for (int i = 0; i < sizeof(animals)/sizeof(animals[0]); i++) {
+		for (int j = 0; j < sizeof(animals[0])/sizeof(string); j++) {
+			cout << animals[i][j] << " " << flush;
+		}
+		cout << endl;
+	}
+
+	return 0;
+
+}
